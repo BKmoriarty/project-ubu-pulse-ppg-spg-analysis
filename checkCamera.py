7864457593b6be4d0d7d56666999ea1b1722cd2c
Teerasak <teer1246@gmail.com>
2024-12-05 16:50:32 +0700

    use new main Pyspin

--- checkCamera.py
+++ checkCamera.py
@@ -13,30 +13,38 @@ import numpy as np
 from datetime import datetime
 
 from SignalExtractionAndAnalysis import Analysis_PPG_SPG
+from recordVideo import RecordVideo
 
 
 class Main():
     def __init__(self, port_name='COM10', exposure_time=2200, name: str = 'unknown', fps=30, size=(600, 600), lenght=20):
-        # self.cap = EasyPySpin.VideoCapture(0)
-        # self.cap.set(cv2.CAP_PROP_EXPOSURE, 1500)
-
+        
         self.frame: tuple = size
         self.fps = fps
         self.length = lenght
         self.ExposureTime = exposure_time
-        self.stop = False
 
         self.current_time = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
-        self.video_path = 'storage/%s %s %s %s %s/video.avi' % (
+        self.video_path = 'storage/%s %s %s %s %s/video' % (
             self.current_time, name, exposure_time, fps, size)
         self.serial_path = 'storage/%s %s %s %s %s/serial.xlsx' % (
             self.current_time, name, exposure_time, fps, size)
-        self.first_frame_time = None
-        self.last_frame_time = None
 
         self.custom_header = ['Data', 'Time', 'Value',
                               'Status', 'Checksum', "Hex Data", "Raw Data"]
 
+        # Initialize RecordVideo instance
+        self.camera = RecordVideo(
+            fps=fps,
+            exposure_time=exposure_time,
+            width=size[0],
+            height=size[1],
+            filename=self.video_path,
+            time_record=lenght
+        )
+        self.camera.init_camera()
+
+        # Serial port setup
         self.port_name = port_name
         self.baudrate = 112500
         self.ser = serial.Serial(port=self.port_name, baudrate=self.baudrate, parity=serial.PARITY_NONE,
@@ -79,151 +87,17 @@ class Main():
         self.ser.close()
         cv2.destroyAllWindows()
 
-    # def save_video(self):
-    #     start_time = time.time()
-    #     # self.current_frame <= self.frame:
-    #     all_frame = []
-    #     while (time.time() - start_time) <= 20:
-    #         ret, frame = self.cap.read()
-    #         if not ret:
-    #             print("Failed to grab frame")
-    #             break
-
-    #         frame = cv2.cvtColor(frame, cv2.COLOR_BayerBG2BGR)
-
-    #         all_frame.append(frame)
-    #         print(time.time() - start_time)
-
-    #         # img_show = cv2.resize(frame, None, fx=0.25, fy=0.25)
-    #         # cv2.imshow("press q to quit", img_show)
-
-    #         self.current_frame += 1
-
-    #         key = cv2.waitKey(1) & 0xFF
-    #         if key == ord("q"):
-    #             break
-
-    #     print(f"png cached : {(time.time() - start_time):.6f}")
-    #     # Save frame as PNG
-    #     # for frame in all_frame:
-    #     #     cv2.imwrite(os.path.join(self.image_folder,
-    #     #                              f'frame_{frame[0]:04d}.png'), frame[1])
-    #     self.stop = True
-    #     cv2.destroyAllWindows()
-    #     print("Png saved")
-
-    #     frame = all_frame[0]
-    #     height, width, layers = frame.shape
-    #     video = cv2.VideoWriter(
-    #         self.video_path, cv2.VideoWriter_fourcc(*'XVID'), 42, (width, height))
-    #     x = 0
-    #     for image in all_frame:
-    #         x += 1
-    #         print(f"image: {x}")
-    #         video.write(image)
-
-    #     video.release()
-    #     print("Video saved")
-
-    def save_video_by_pyspin(self):
-        time.sleep(1)
-        with Camera() as cam:
-            cam.Width = self.frame[0]
-            cam.Height = self.frame[1]
-
-            cam.OffsetX = (cam.SensorWidth - cam.Width) // 2
-            cam.OffsetY = (cam.SensorHeight - cam.Height) // 2
-
-            cam.AcquisitionFrameRateAuto = 'Off'
-            cam.AcquisitionFrameRateEnabled = True
-            cam.AcquisitionFrameRate = self.fps
-
-            cam.ExposureAuto = 'Off'
-            cam.ExposureTime = self.ExposureTime
-
-            start_time = time.time()
-            print(f"record camera :{pd.Timestamp.now().time()}")
-            cam.start()
-            # imgs = [cam.get_array() for n in range(600)]
-            # List to store images and their timestamps
-            imgs_with_timestamps = []
-            temp = []
-
-            for n in range(self.length*self.fps):
-                img_array = cam.get_array()
-                temp.append(img_array)
-                timestamp = datetime.timestamp(datetime.now())
-                imgs_with_timestamps.append((timestamp, img_array))
-
-                if n == 0:
-                    self.first_frame_time = timestamp
-                self.last_frame_time = timestamp
-
-            cam.stop()
-            end_time = time.time()
-
-            np.save('temp\img_array.npy', img_array)
-            print(f'Time Record : {(end_time-start_time):.6f} Second.')
-
-        time.sleep(1)
-        self.stop = True
-
-        # for n, img in enumerate(imgs):
-        #     Image.fromarray(img).save(os.path.join(
-        #         self.image_folder, '%08d.png' % n))
-
-        for timestamp, img_array in imgs_with_timestamps:
-            filename = os.path.join(self.image_folder, '%s.png' % timestamp)
-            Image.fromarray(img_array).save(filename)
-
-    def convert_frames_to_video(self):
-        images = [img for img in os.listdir(
-            self.image_folder) if img.endswith(".png")]
-        images.sort()
-
-        frame = cv2.imread(os.path.join(self.image_folder, images[0]))
-        height, width, layers = frame.shape
-
-        video = cv2.VideoWriter(
-            self.video_path, cv2.VideoWriter_fourcc(*'XVID'), self.fps, (width, height))
-
-        for image in images:
-            # print(f"image: {x}")
-            # img = cv2.imread(os.path.join(self.image_folder, image))
-            # timestamp = pd.Timestamp.now().time()
-            # cv2.putText(img, f"Time: {timestamp}", (10, 30),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-            # video.write(img)
-            # Extract timestamp from filename
-            timestamp_str = os.path.splitext(image)[0]
-            try:
-                # Convert timestamp_str to a readable format
-                timestamp = datetime.fromtimestamp(
-                    float(timestamp_str)).strftime('%Y-%m-%d %H:%M:%S.%f')
-            except ValueError:
-                print(timestamp_str)
-                timestamp = "Invalid Timestamp"
-
-            img = cv2.imread(os.path.join(self.image_folder, image))
-            # Overlay the timestamp on the image
-            # cv2.putText(img, f"Time: {timestamp}", (10, 30),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-            video.write(img)
-
-        video.release()
-        print("Video saved")
-
     def save_serial(self):
         serial_data = []
         start_time = time.time()
         print(f"record serial {pd.Timestamp.now().time()}")
-        while (not self.stop):
-            if self.ser.in_waiting > 0 and not self.stop:
+        while (not self.camera.stop):
+            if self.ser.in_waiting > 0 and not self.camera.stop:
                 data = self.ser.read(100)  # Read 100 bytes or adjust as needed
                 packets = [data[i:i+4]
                            for i in range(0, len(data), 4)]
                 for packet in packets:
-                    if packet[0] == 0xFF and not self.stop:
+                    if packet[0] == 0xFF and not self.camera.stop:
                         value = packet[1]
                         status = packet[2]
                         checksum = packet[3]
@@ -236,18 +110,22 @@ class Main():
 
         print(f'Time Excel : {(time.time()-start_time):.6f} Second.')
         if len(serial_data) > 0:
-            df = pd.DataFrame(serial_data, columns=self.custom_header)
+            df = pd.DataFrame(serial_data, columns=pd.Index(self.custom_header))
 
             # Time type to float
             df['Time'] = df['Time'].astype(float)
 
-            print(f"first_frame_time: {self.first_frame_time}")
+            print(f"first_frame_time: {self.camera.first_frame_time}")
             print(f'first_row_time: {df.iloc[0]["Time"]}')
-            print(f"last_frame_time: {self.last_frame_time}")
+            print(f"last_frame_time: {self.camera.last_frame_time}")
             print(f'last_row_time: {df.iloc[-1]["Time"]}')
 
-            start_date = float(self.first_frame_time)
-            end_date = float(self.last_frame_time)
+            if self.camera.first_frame_time == None or self.camera.last_frame_time == None:
+                print("Error: first_frame_time or last_frame_time is None")
+                return
+
+            start_date = float(self.camera.first_frame_time)
+            end_date = float(self.camera.last_frame_time)
 
             filtered_df = df[(df['Time'] >= start_date)
                              & (df['Time'] <= end_date)]
@@ -264,45 +142,15 @@ class Main():
 
         print("Serial data saved")
 
-    def show_real_time_video(self):
-        with Camera() as cam:
-            cam.Width = self.frame[0]
-            cam.Height = self.frame[1]
-
-            print(
-                f"SensorWidth: {cam.SensorWidth}, SensorHeight: {cam.SensorHeight}")
-
-            cam.OffsetX = (cam.SensorWidth - cam.Width) // 2
-            cam.OffsetY = (cam.SensorHeight - cam.Height) // 2
-
-            cam.AcquisitionFrameRateAuto = 'Off'
-            cam.AcquisitionFrameRateEnabled = True
-            cam.AcquisitionFrameRate = self.fps
-
-            cam.ExposureAuto = 'Off'
-            cam.ExposureTime = self.ExposureTime
-
-            cam.start()
-
-            while True:
-                img_array = cam.get_array()
-
-                cv2.imshow('Real Time Video', img_array)
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-            cam.stop()
-            cv2.destroyAllWindows()
-
+    
+        
     def run(self):
-        self.show_real_time_video()
+        self.camera.display_images()
 
         os.makedirs(
             f'storage/{self.current_time} {name} {exposure_time} {fps} {self.frame}', exist_ok=True)
 
-        thread1 = threading.Thread(
-            target=self.save_video_by_pyspin, name='video')
+        thread1 = threading.Thread(target=self.camera.start, name='camera')
         thread2 = threading.Thread(target=self.save_serial, name='serial')
 
         thread1.start()
@@ -311,10 +159,7 @@ class Main():
         thread1.join()
         thread2.join()
 
-        if self.stop:
-            self.convert_frames_to_video()
-
-        return self.video_path, self.serial_path
+        return self.video_path+"-0000.avi", self.serial_path
 
 
 def calculate_exposure_time(new_fps: int = 88):
@@ -378,9 +223,9 @@ if __name__ == '__main__':
     size_cam = (200, 200)  # W x H
     size_ppg = 200  # W x H
     size_spg = 100  # W x H
-    fps = 122  # Hz
-    exposure_time = 6000
-    # calculate_exposure_time(fps)
+    fps = 350  # Hz
+    exposure_time = 1200
+    # int(calculate_exposure_time(fps))
     cache = False
 
     length = 20  # second
